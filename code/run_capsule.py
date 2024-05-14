54e17100d31e4617806c45cc0f6d43584f208356
Arjun Sridhar <arjun.sridhar@alleninstitute.org>
2024-05-14 19:01:04 +0000

    Edited run_capsule.py, utils.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,33 +1,59 @@
+"""
+Capsule that performs temporal and spaitial subsampling of LFP data. Default behavior ported from allen sdk.
+Temporal Subsample by 2 and Spaitial Channel Subsample by taking every 4th channel. Can overwrite these with input args
+"""
+
 import pathlib
 import spikeinterface as si
 import spikeinterface.preprocessing as sip
 import utils
+import argparse
 
 DATA_PATH = pathlib.Path('/data')
 RESULTS_PATH = pathlib.Path('/results')
-# TODO: move to input args
+
 TEMPORAL_SUBSAMPLE_FACTOR = 2 
 SPAITIAL_CHANNEL_SUBSAMPLE_FACTOR = 4
 IS_AVERAGE_DIRECTION = False
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--TEMPORAL_SUBSAMPLE_FACTOR', help='Value to subsample time dimension of LFP')
+parser.add_argument('--SPAITIAL_CHANNEL_SUBSAMPLE_FACTOR', help='Value to subsample channel dimension of LFP')
+parser.add_argument('--IS_AVERAGE_DIRECTION', help='whether or not to apply average direction across same channel positions in y direction')
+
 def run():
+    args = parser.parse_args()
+
+    if args.TEMPORAL_SUBSAMPLE_FACTOR:
+        TEMPORAL_SUBSAMPLE_FACTOR = args.TEMPORAL_SUBSAMPLE_FACTOR
+    
+    if args.SPAITIAL_CHANNEL_SUBSAMPLE_FACTOR:
+        SPAITIAL_CHANNEL_SUBSAMPLE_FACTOR = args.SPAITIAL_CHANNEL_SUBSAMPLE_FACTOR
+    
+    if args.IS_AVERAGE_DIRECTION:
+        IS_AVERAGE_DIRECTION = args.IS_AVERAGE_DIRECTION
 
     session_id = utils.parse_session_id()
 
     zarr_lfp_paths = tuple(DATA_PATH.glob('*/ecephys_compressed/*-LFP.zarr'))
+    if not zarr_lfp_paths:
+        raise FileNotFoundError(f'No compressed lfp data found for session {session_id}')
 
     for lfp_path in zarr_lfp_paths:
         recording = si.read_zarr(lfp_path)
-        if IS_AVERAGE_DIRECTION: # TODO add average direction
-            pass
 
         channel_ids = recording.get_channel_ids()
         channel_ids_to_keep = [channel_ids[i] for i in range(0, len(channel_ids), SPAITIAL_CHANNEL_SUBSAMPLE_FACTOR)] 
         channel_ids_to_remove = [channel_ids[i] for i in range(0, len(channel_ids)) if channel_ids[i] not in channel_ids_to_keep]
 
         recording_channels_removed = recording.remove_channels(channel_ids_to_remove)
-        resampled_recording = sip.resample(recording_channels_removed, int(recording.sampling_frequency / RESAMPLE_RATE))
+        resampled_recording = sip.resample(recording_channels_removed, int(recording.sampling_frequency / TEMPORAL_SUBSAMPLE_FACTOR))
 
+        if IS_AVERAGE_DIRECTION:
+            resampled_recording = sip.AverageAcrossDirectionRecording(resampled_recording)
+
+        assert (len(resampled_recording.get_times()) == len(recording.get_times()) / TEMPORAL_SUBSAMPLE_FACTOR
+        ), f"Applying {TEMPORAL_SUBSAMPLE_FACTOR} temporal factor resulted in mismatch downsampling. Got {len(resampled_recording.get_times())} time samples given {len(recording.get_times())} raw time samples and factor {TEMPORAL_SUBSAMPLE_FACTOR}"
         assert (resampled_recording.get_num_channels() == recording.get_num_channels() / SPAITIAL_CHANNEL_SUBSAMPLE_FACTOR
         ), f"Applying {SPAITIAL_CHANNEL_SUBSAMPLE_FACTOR} channel stride resulted in mismatch downsampling {recording.get_num_channels()} channels and {resampled_recording.get_num_channels()} channels"
 
--- code/utils.py
+++ code/utils.py
@@ -11,10 +11,10 @@ def check_saved_subsampled_lfp_result(result_output_path: pathlib.Path, input_da
     subsampled_lfp = zarr.open(result_output_path, mode='r')['traces_seg0']
 
     assert (subsampled_lfp.shape[0] == raw_lfp.shape[0] / temporal_subsampling_factor
-    ), f"Temporal subsampling mismatch. Got subsampled time samples {subsampled_lfp.shape[0]} given raw time samples {raw_lfp.shape[0]} and temporal factor {temporal_subsampling_factor}"
+    ), f"Temporal subsampling mismatch after saving to zarr. Got subsampled time samples {subsampled_lfp.shape[0]} given raw time samples {raw_lfp.shape[0]} and temporal factor {temporal_subsampling_factor}"
 
     assert (subsampled_lfp.shape[1] == raw_lfp.shape[1] / spaital_channel_subsampling_factor
-    ), f"Spaital subsampling mismatch. Got subsampled number of channels {subsampled_lfp.shape[1]} given raw number of channels {raw_lfp.shape[1]} and spaitial factor {spaital_channel_subsampling_factor}"
+    ), f"Spaital subsampling mismatch after saving to zarr. Got subsampled number of channels {subsampled_lfp.shape[1]} given raw number of channels {raw_lfp.shape[1]} and spaitial factor {spaital_channel_subsampling_factor}"
 
 def parse_session_id() -> str:
     """
@@ -27,7 +27,7 @@ def parse_session_id() -> str:
     
     session_id = None
     for session_path in session_paths:
-        try: # avoid parsing model folder, better way to do this?
+        try:
             session_id = npc_session.parsing.extract_aind_session_id(session_path.stem)
         except ValueError:
             pass
@@ -36,19 +36,6 @@ def parse_session_id() -> str:
         raise FileNotFoundError('No data asset attached that follows aind session format')
     
     return session_id
-
-def write_json_with_session_id() -> None:
-    """
-    Test assumes data asset SmartSPIM_695464_2023-10-18_20-30-30 is attached
-    >>> write_json_with_session_id()
-    >>> path = tuple(RESULTS_PATH.glob('*.json'))[0]
-    >>> path.as_posix()
-    '/results/SmartSPIM_695464_2023-10-18_20-30-30.json'
-    """
-    session_id = parse_session_id()
-    session_dict = {'session_id': session_id}
-    with open(RESULTS_PATH / f'{session_id}.json', 'w') as f:
-        json.dump(session_dict, f)
 
 if __name__ == '__main__':
     import doctest
